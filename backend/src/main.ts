dc30eee53db32803158b855da484e364ae99b877
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2026-06-17 23:30:45 +0100

    backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-  }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Product Reservation API')
+    .setDescription(
+      'API for managing products, reservations, checkout, and inventory.',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -1,24 +1,48 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Param, Get, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
-import { Param, Get, Query } from '@nestjs/common';
 import { ProductQueryDto } from './dto/product-query.dto';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBody,
+  ApiParam,
+  ApiQuery,
+} from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a product' })
+  @ApiBody({ type: CreateProductDto })
+  @ApiResponse({ status: 201, description: 'Product created successfully' })
   create(@Body() dto: CreateProductDto) {
     return this.productsService.create(dto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a product by ID' })
+  @ApiParam({
+    name: 'id',
+    example: '2736ba17-4f5f-4b7f-9ccf-eab127791384',
+  })
+  @ApiResponse({ status: 200, description: 'Product retrieved successfully' })
   findOne(@Param('id') id: string) {
     return this.productsService.findOne(id);
-}
+  }
 
   @Get()
+  @ApiOperation({ summary: 'Get all products' })
+  @ApiQuery({ name: 'page', required: false, example: 1 })
+  @ApiQuery({ name: 'limit', required: false, example: 20 })
+  @ApiQuery({ name: 'sort', required: false, example: 'createdAt' })
+  @ApiQuery({ name: 'stock', required: false, example: 'gt:0' })
+  @ApiResponse({ status: 200, description: 'Products retrieved successfully' })
   findAll(@Query() query: ProductQueryDto) {
     return this.productsService.findAll(query);
   }
