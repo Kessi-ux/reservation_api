d822fbd711acd532041fcaac84f37b5cc8767fb9
Kessi-ux <chukwubueze.uchenyi.231361@unn.edu.ng>
2026-06-18 12:09:01 +0100

    fix:database config error && stop tracking dist and stale tsbuildinfo cache

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,9 +7,20 @@ import { ReservationsModule } from './reservations/reservations.module';
 import { OrdersModule } from './orders/orders.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule, ProductsModule, ReservationsModule, OrdersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    UsersModule,
+    ProductsModule,
+    ReservationsModule,
+    OrdersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
